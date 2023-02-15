2261267a9918e8299bdb88dc6ba83f3b2cd6fbaf
ismaelcoulibaly <@ismael>
2023-02-14 23:40:08 -0500

    ..

--- src/components/add-tutorial.component.js
+++ src/components/add-tutorial.component.js
@@ -20,51 +20,51 @@ export default class AddTutorial extends Component{
             submitted: false
         };
     }
-
-    onChangeTitle(e) {
-        this.setState({
-            title: e.target.value
-        });
-    }
-
-    onChangeDescription(e) {
-        this.setState({
-            description: e.target.value
-        });
-    }
-
-    saveTutorial() {
-        var data = {
-            title: this.state.title,
-            description: this.state.description
-        };
-
-        TutorialDataService.create(data)
-            .then(response => {
-                this.setState({
-                    id: response.data.id,
-                    title: response.data.title,
-                    description: response.data.description,
-                    published: response.data.published,
-
-                    submitted: true
-                });
-                console.log(response.data);
-            })
-            .catch(e => {
-                console.log(e);
-            });
-    }
-
-    newTutorial() {
-        this.setState({
-            id: null,
-            title: "",
-            description: "",
-            published: false,
-
-            submitted: false
-        });
-    }
+    //
+    // onChangeTitle(e) {
+    //     this.setState({
+    //         title: e.target.value
+    //     });
+    // }
+    //
+    // onChangeDescription(e) {
+    //     this.setState({
+    //         description: e.target.value
+    //     });
+    // }
+    //
+    // saveTutorial() {
+    //     var data = {
+    //         title: this.state.title,
+    //         description: this.state.description
+    //     };
+    //
+    //     TutorialDataService.create(data)
+    //         .then(response => {
+    //             this.setState({
+    //                 id: response.data.id,
+    //                 title: response.data.title,
+    //                 description: response.data.description,
+    //                 published: response.data.published,
+    //
+    //                 submitted: true
+    //             });
+    //             console.log(response.data);
+    //         })
+    //         .catch(e => {
+    //             console.log(e);
+    //         });
+    // }
+    //
+    // newTutorial() {
+    //     this.setState({
+    //         id: null,
+    //         title: "",
+    //         description: "",
+    //         published: false,
+    //
+    //         submitted: false
+    //     });
+    // }
 
 }
